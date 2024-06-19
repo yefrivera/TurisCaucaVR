d262b55bfcc6ec66542d6f69a53bc5d5a94e4a81
yefrivera <yvera@unicauca.edu.co>
2024-06-19 13:12:44 -0500

    menu

---
+++ menu.js
@@ -0,0 +1,100 @@
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+hamMenu.classList.toggle("active");
+offScreenMenu.classList.toggle("active");
+});
+
+//--------------------------------------
+
+//-------------------------funciones del menú-----------------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+    var expandBtn = document.getElementById('expand-btn');
+    var expandBtnImg = expandBtn.querySelector('img');
+            
+    const muteBtn = document.getElementById('mute-btn');
+
+    // Agregar un evento de clic al botón "muted" para controlar el estado de silencio del video y cambiar la imagen
+    muteBtn.addEventListener('click', function() {
+        if (video.muted) {
+            video.muted = false;
+            // Cambiar la imagen a "unmuted"
+            muteBtn.querySelector('img').src = muteBtn.querySelector('img').getAttribute('data-original-src');
+        } else {
+            video.muted = true;
+            // Cambiar la imagen a "muted"
+            muteBtn.querySelector('img').src = muteBtn.querySelector('img').getAttribute('data-alt-src');
+        }
+    });
+
+    //---------------------------------------------
+
+    expandBtn.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default behavior of the link
+        var element = document.documentElement;
+
+        if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+            exitFullscreen();
+        } else {
+            enterFullscreen();
+        }
+    });
+
+    function enterFullscreen() {
+        var element = document.documentElement;
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+
+    function exitFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
+    document.addEventListener('mozfullscreenchange', handleFullscreenChange);
+    document.addEventListener('MSFullscreenChange', handleFullscreenChange);
+
+    function handleFullscreenChange() {
+        var isFullscreen = document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement;
+
+        if (isFullscreen) {
+            expandBtnImg.src = expandBtnImg.getAttribute('data-alt-src');
+        } else {
+            expandBtnImg.src = expandBtnImg.getAttribute('data-original-src');
+        }
+    }
+
+    
+
+
+    document.querySelector('.share-x').addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+    window.open('https://x.com/share?url=' + encodeURIComponent(window.location.href), 'Compartir en Twitter', 'width=600,height=400');
+    });
+
+    document.querySelector('.share-facebook').addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(window.location.href), 'Compartir en Facebook', 'width=600,height=400');
+    });
+
+});
--- tour.js
+++ tour.js
@@ -16,40 +16,58 @@ const panorama4 = new PANOLENS.ImagePanorama('./images/bancobogota.jpg');
 const panorama5 = new PANOLENS.ImagePanorama('./images/juanvaldez.jpg');
 const panorama6 = new PANOLENS.ImagePanorama('./images/catt.jpg');
 
+const panorama7 = new PANOLENS.ImagePanorama('./images/reloj.jpg');
+const panorama8 = new PANOLENS.ImagePanorama('./images/esq-b.jpg');
+const panorama9 = new PANOLENS.ImagePanorama('./images/esq-bb.jpg');
+const panorama10 = new PANOLENS.ImagePanorama('./images/esq-c.jpg');
 
+const panorama11 = new PANOLENS.ImagePanorama('./images/humilladero1.jpg');
 //------------------------------
 
 //-------------------------------
 panorama1.link(panorama2, new THREE.Vector3(-800, -1500, 6000)); 
 panorama1.link(panorama3, new THREE.Vector3(-6000, -1500, -190));
 panorama1.link(panorama4, new THREE.Vector3(0, -1800, -6000)); 
-panorama1.link(panorama5, new THREE.Vector3(5000, -700, 1350));
+panorama1.link(panorama5, new THREE.Vector3(6000, -800, 1600));
 
 panorama2.link(panorama1, new THREE.Vector3(-6200, -1800, -2000));
-panorama2.link(panorama3, new THREE.Vector3(-2400, -1000, 6000));
-panorama2.link(panorama5, new THREE.Vector3(1500, -1000, -5000));
+panorama2.link(panorama10, new THREE.Vector3(-2400, -1000, 6000));
+panorama2.link(panorama7, new THREE.Vector3(1500, -1000, -5000));
 panorama2.link(panorama6, new THREE.Vector3(7200, -1000, 2550));
 
-
 panorama3.link(panorama1, new THREE.Vector3(6000, -1800, 2300));
-panorama3.link(panorama2, new THREE.Vector3(-1500, -550, 6000));
-panorama3.link(panorama4, new THREE.Vector3(1500, -1000, -7000));
+panorama3.link(panorama10, new THREE.Vector3(-1500, -550, 6000));
+panorama3.link(panorama9, new THREE.Vector3(1500, -1000, -7000));
 
 panorama4.link(panorama1, new THREE.Vector3(6000, -1000, 1400));
-panorama4.link(panorama3, new THREE.Vector3(-1000, -1000, 6000));
-panorama4.link(panorama5, new THREE.Vector3(2000, -1000, -7000));
-
+panorama4.link(panorama9, new THREE.Vector3(-1000, -1000, 6000));
+panorama4.link(panorama8, new THREE.Vector3(2000, -1000, -7000));
 
 panorama5.link(panorama1, new THREE.Vector3(5000, -1000, -400));
-panorama5.link(panorama2, new THREE.Vector3(0, -1000, -6500));
-panorama5.link(panorama4, new THREE.Vector3(1200, -1000, 6000));
+panorama5.link(panorama7, new THREE.Vector3(0, -1000, -6500));
+panorama5.link(panorama8, new THREE.Vector3(1200, -1000, 6000));
+
+panorama7.link(panorama2, new THREE.Vector3(4000, -1000, -6000));
+panorama7.link(panorama5, new THREE.Vector3(3900, -1000, 6000));
 
 panorama6.link(panorama2, new THREE.Vector3(-7200, -2000, 850));
+
+panorama8.link(panorama4, new THREE.Vector3(3450, -1000, 5000));
+panorama8.link(panorama5, new THREE.Vector3(5500, -1000, -4400));
+
+panorama9.link(panorama3, new THREE.Vector3(3500, -1000, 5000));
+panorama9.link(panorama4, new THREE.Vector3(5300, -1000, -4000));
+panorama9.link(panorama11, new THREE.Vector3(-5500, -1400, -3000));
+
+panorama10.link(panorama2, new THREE.Vector3(1100, -1000, 6000));
+panorama10.link(panorama3, new THREE.Vector3(5800, -1000, -3500));
+
+panorama11.link(panorama3, new THREE.Vector3(-1300, -1500, 6000));
 
 
 // Agregar panoramas al visor
 viewer.add(panorama1, panorama2, panorama3, panorama4, panorama5,panorama6);
-
+viewer.add(panorama7,panorama8,panorama9,panorama10,panorama11);
 // Iniciar con el primer panorama
 viewer.setPanorama(panorama1);
 
